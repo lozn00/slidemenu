8b74564fbd0d2dc72dc9e357e5648627029f1b69
luozheng <694886526@qq.com>
2017-06-27 22:55:18 +0800

    冲突还没有解决，未完待续。

--- app/src/main/java/space/qssq/dragdemo/VerticalLPager.java
+++ app/src/main/java/space/qssq/dragdemo/VerticalLPager.java
@@ -3,6 +3,8 @@ package space.qssq.dragdemo;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
+import android.support.v4.view.NestedScrollingParent;
+import android.support.v4.view.NestedScrollingParentHelper;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
@@ -16,7 +18,7 @@ import android.widget.FrameLayout;
  * 默认显示2个封面
  * 把封面往下面拖将会隐藏 隐藏之后往上面拉又会被拉出来
  */
-public class VerticalLPager extends FrameLayout {
+public class VerticalLPager extends FrameLayout  implements NestedScrollingParent{
 
     private static final String TAG = "VerticalLPager";
     private float criticalVel;
@@ -25,6 +27,7 @@ public class VerticalLPager extends FrameLayout {
     private View mCoverView;
     private int mSize;
     private int mPointerId;
+    private NestedScrollingParentHelper parentHelper;
 
     public VerticalLPager(Context context) {
         super(context);
@@ -42,8 +45,110 @@ public class VerticalLPager extends FrameLayout {
         init(context);
     }
 
+    @Override
+    public void onNestedScroll(View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+    }
 
-//    class YScrollDetector extends GestureDetector.SimpleOnGestureListener {
+    /*
+
+        *    在目标视图消耗卷的一部分之前，对正在进行中的嵌套滚动作出响应。
+        *
+        *    当使用嵌套滚动时，父视图通常需要一个机会。
+        *    在嵌套滚动子操作之前消耗滚动条。这是一个例子
+        *    抽屉里包含一个可滚动列表。用户希望能够滚动列表。
+        *    在列表本身开始滚动之前完全进入视图。
+        *
+        *     <BR> <代码> > < / onnestedprescroll代码时调用嵌套调用滚动的孩子
+        *     {@链接查看# dispatchnestedprescroll（int，int，int，int []，[]）}。实施应
+        *    报告DX中所报告的滚动条的像素是如何被消耗的
+        *     <代码>消耗<代码>数组。指标0对应DX和指数1对应了。
+        *    这个参数永远不会为空。消耗[ 0 ]和消耗[ 1 ]的初始值
+        *    永远是0。
+        *
+        *     @param目标视图启动嵌套滚动
+        *     @param DX水平滚动的距离，以像素为单位
+        *     @param DY垂直滚动的距离的像素
+        *     @param消耗输出。水平和垂直滚动的距离
+     */
+    @Override
+    public void onNestedPreScroll(View target, int dx, int dy, int[] consumed) {
+        Log.w(TAG,"onNestedPreScroll dx "+dx+", dy:"+dy+",consumed:");
+        super.onNestedPreScroll(target, dx, dy, consumed);
+    }
+    /**
+     请求从嵌套滚动抛出。
+     *
+     *此方法表示嵌套滚动子已检测到合适的条件。
+     *一时冲动。一般来说，这意味着一个触摸滚动已经结束了
+     * { } velocitytracker速度的方向滚动，达到或超过
+     * viewconfiguration # getscaledminimumflingvelocity()最小投掷速度}
+     *沿滚动轴。</P >
+     *
+     *如果嵌套滚动子视图通常抛出，但它位于
+     *它自己的内容，它可以使用这个方法委派抛出到它的嵌套滚动。
+     *父母代替。家长可随意选择掷球或观察儿童投掷。
+     *
+     * @param目标视图启动嵌套滚动
+     * @param隙气速的水平速度在每秒的像素
+     * @paramvelocityy垂直速度在每秒的像素
+     * @param消耗如果孩子消耗的放纵，否则为假
+     *
+     * @param target View that initiated the nested scroll
+     * @param velocityX Horizontal velocity in pixels per second
+     * @param velocityY Vertical velocity in pixels per second
+     * @param consumed true if the child consumed the fling, false otherwise
+     * @return true if this parent consumed or otherwise reacted to the fling
+     */
+
+    /**
+     *在目标视图使用它之前对嵌套的抛出做出反应。
+     *
+     * <BR>这种方法siginfies，嵌套滚动的孩子已经检测到一个扔了
+     *沿每个轴的速度。一般来说，这意味着一个触摸滚动已经结束了
+     * {@链接velocitytracker速度}的方向滚动，达到或超过
+     * {@链接viewconfiguration # getscaledminimumflingvelocity()最小投掷速度}
+     *沿滚动轴。</P >
+     *
+     *如果嵌套滚动的父进程正在消耗运动作为
+     * {@链接# onnestedprescroll（int，int，int，[]）预滚动}，它可能是适当的
+     *它也消耗前投掷完成相同的议案。通过返回
+     *此方法中的代码<真<代码>，父指示子不应该
+     *也有自己的内部内容。
+     *
+     * @param目标视图启动嵌套滚动
+     * @param隙气速的水平速度在每秒的像素
+     * @paramvelocityy垂直速度在每秒的像素
+     *如果该父项消耗了目标视图前面的抛出，则返回true。
+     *
+     * @param target View that initiated the nested scroll
+     * @param velocityX Horizontal velocity in pixels per second
+     * @param velocityY Vertical velocity in pixels per second
+     * @return true if this parent consumed the fling ahead of the target view
+     */
+
+    /**
+     *返回当前轴嵌套滚动这nestedscrollingparent。
+     *
+     * <BR>一nestedscrollingparent返回其他东西{@链接viewcompat # scroll_axis_none }
+     *当前作为一个或多个后代视图的嵌套滚动母函数。
+     *层次结构。
+     *
+     *表示当前嵌套滚动轴的返回标志
+     *“看viewcompat # scroll_axis_horizontal
+     *“看viewcompat # scroll_axis_vertical
+     *“看viewcompat # scroll_axis_none
+     * @see ViewCompat#SCROLL_AXIS_HORIZONTAL
+     * @see ViewCompat#SCROLL_AXIS_VERTICAL
+     * @see ViewCompat#SCROLL_AXIS_NONE
+     */
+    public int getNestedScrollAxes(){
+        return parentHelper.getNestedScrollAxes();
+    }
+
+
+
+    //    class YScrollDetector extends GestureDetector.SimpleOnGestureListener {
 //        @Override
 //        public boolean onScroll(MotionEvent e1, MotionEvent e2, float dx, float dy) {
 //            return Math.abs(dy) <= Math.abs(dx);
@@ -53,6 +158,7 @@ public class VerticalLPager extends FrameLayout {
 
     public void init(Context context) {
         criticalVel = getResources().getDisplayMetrics().density * 120;
+        parentHelper = new NestedScrollingParentHelper(this);
 //        gestureDetector = new GestureDetector(getContext(), new GestureDetector.OnGestureListener() {
 //            @Override
 //            public boolean onDown(MotionEvent e) {
